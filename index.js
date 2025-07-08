a9d7b7d614ca85e8fff655c5592f7347edc67ca6
samuellssl <samuell.santos@hotmail.com>
2025-07-08 00:06:13 -0300

    finalizacao

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("oi")
+// variável nome do player
+
+let nome = "Maluket22"
+
+// variável level do player
+
+let levelPlayer = 40000
+
+// variável clássificação de level
+
+let level;
+
+// condição
+
+if(levelPlayer <= 500){
+    level = "Bronze";
+} else if(levelPlayer <= 1000){
+    level = "Prata";
+} else if(levelPlayer <= 2000){
+    level = "Ouro";
+} else{
+    level = "Imortal";
+}
+
+console.log("O heroi de nome " + nome + "está no nível " + level);
